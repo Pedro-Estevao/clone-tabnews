e32734f5856dca5b59f320ec0567148e6487cd6e
Pedro Estevão <pedropaulista11@gmail.com>
2025-05-05 20:01:21 -0300

    Add scripts `lint:check` and `lint:fix`

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-		suppressHydrationWarning
+        suppressHydrationWarning
       >
         {children}
       </body>
